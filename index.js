669009b28b330179d832c70b93cb07e33b1135b3
Pravesh Yadav <praveshyadav6307@gmail.com>
2026-05-06 17:14:51 +0530

    Updated port

--- index.js
+++ index.js
@@ -542,6 +542,10 @@ app.get('/', requireLogin, async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => { console.log(`📊 Admin Vault Dashboard is LIVE on Port 3000`); });
+
+// '0.0.0.0' daalne se dashboard aapke laptop ke IP se bhi khulega
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`🚀 DASHBOARD READY: http://192.168.31.183:${PORT}`);
+});;
 
 client.login(process.env.DISCORD_TOKEN);
