4df0ffa9fc2d227e3049f9d6f9fe797a1c47a214
Pravesh Yadav <praveshyadav6307@gmail.com>
2026-09-10 08:11:11 +0530

    Fixed

--- index.js
+++ index.js
@@ -264,7 +264,7 @@ client.on('messageCreate', async (message) => {
     // ==========================================
     // 👑 ADMIN & BOSS PROTOCOL CHECK
     // ==========================================
-    const adminDiscordIds = ['1001128047128358923', '1336703883711479896']; 
+    const adminDiscordIds = ['1001128047128358923', '1336703883711479896', '1541859306050162750']; 
     const isAuthorAdmin = adminDiscordIds.includes(message.author.id);
 
     // ==========================================
